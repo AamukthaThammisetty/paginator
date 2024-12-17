9ae7a35d6d8529ea9d1ed7a214aafa1b9e633d35
Aamuktha <aa>
2024-12-17 16:26:04 +0530

    imitialized project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Table from './table'
+import 'primereact/resources/themes/lara-light-cyan/theme.css'
+
+function App() {
+  return <Table />
+}
+
+export default App
