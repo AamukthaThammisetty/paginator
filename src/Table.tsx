221f58d9662fb6b4a3d48e5f46af0b0c45944855
Aamuktha <aa>
2024-12-17 17:54:26 +0530

    added multi selection

--- src/Table.tsx
+++ src/Table.tsx
@@ -11,15 +11,33 @@ import { FaChevronDown } from 'react-icons/fa'
 
 export default function Table() {
   const [page, setPage] = useState(1)
+  const [data, setData] = useState([])
+  const [selectedRows, setSelectedRows] = useState<any[]>([])
   const [rowInput, setRowInput] = useState(0)
+  const [totalRecords, setTotalRecords] = useState(0)
   const rows = 12
+  useEffect(() => {
+    axios.get(`https://api.artic.edu/api/v1/artworks?page=${page}`).then((res) => {
+      setData(res.data.data)
+      setTotalRecords(res.data.pagination.total)
+    })
+  }, [page])
   const onPageChange = (e: { page: number }) => {
     setPage(e.page + 1)
   }
   return (
     <div className="p-20 ">
       <PrimeReactProvider>
-        <DataTable rows={12} stripedRows dataKey="id" className="custom-table" tableStyle={{ minWidth: '50rem' }}>
+        <DataTable
+          value={data}
+          rows={12}
+          stripedRows
+          selection={selectedRows}
+          onSelectionChange={(e) => setSelectedRows(e.value)}
+          dataKey="id"
+          className="custom-table"
+          tableStyle={{ minWidth: '50rem' }}
+        >
           <Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column>
           <Column field="place_of_origin" header="Place of Origin" style={{ width: '25%' }}></Column>
           <Column field="artist_display" header="Artist Display" style={{ width: '25%' }}></Column>
